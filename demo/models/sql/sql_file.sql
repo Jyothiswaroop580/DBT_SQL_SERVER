e72b2ff3f2ac4652fcda16b3e91fc1cc29e97d99
Jyothiswaroop580 <swaroopkantipudi5@gmail.com>
2024-09-21 19:27:37 +0530

    incremental sql files

--- demo/models/sql/sql_file.sql
+++ demo/models/sql/sql_file.sql
@@ -1,4 +1,17 @@
 --select top (50) * from [AdventureWorks2022].[HumanResources].[EmployeePayHistory]
-
+{{
+    config(
+        materialized='incremental'
+    )
+}}
 
 select * from {{ source('source', 'JobCandidate') }}
+
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  -- (uses >= to include records whose timestamp occurred since the last run of this model)
+  -- (If event_time is NULL or the table is truncated, the condition will always be true and load all records)
+where ModifiedDate >= (select coalesce(max(ModifiedDate),'1900-01-01') from {{ this }} )
+
+{% endif %}
